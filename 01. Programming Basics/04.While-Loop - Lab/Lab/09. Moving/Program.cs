c6db1555c6d5ec17aeb6915961ccef48c664f6c1
SlaviSavov <slavisavov1@icloud.com>
2019-05-15 21:14:32 +0300

    Added homework

---
+++ 01. Programming Basics/04.While-Loop - Lab/Lab/09. Moving/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _09._Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine());
+            double l = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double room = w * l * h;
+            double box = 0;
+
+            while (room >= box)
+            {
+                string prom = Console.ReadLine();
+                if (prom == "Done")
+                {
+                    Console.WriteLine($"{room - box} Cubic meters left.");
+                    return;
+                }
+                int boxs = int.Parse(prom);
+                box += boxs;
+            }
+            Console.WriteLine($"No more free space! You need {box - room} Cubic meters more.");
+        }
+    }
+}
