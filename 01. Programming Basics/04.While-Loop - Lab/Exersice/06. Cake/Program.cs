33cec39f6d8a32eae1bd22b3bb04356989bad7a5
SlaviSavov <slavisavov1@icloud.com>
2019-05-16 10:49:26 +0300

    added homework

---
+++ 01. Programming Basics/04.While-Loop - Lab/Exersice/06. Cake/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+            int cake = w * l;
+            int piece = 0;
+
+            while (true)
+            {
+                string input1 = Console.ReadLine();
+
+                if (input1 == "STOP" && cake >= piece)
+                {
+                    Console.WriteLine($"{cake - piece} pieces are left.");
+                    return;
+                }
+                else if (cake < piece)
+                {
+                    Console.WriteLine($"No more cake left! You need {piece - cake} pieces more.");
+                    return;
+                }
+                int input = int.Parse(input1);
+                piece += input;
+            }
+    }
+}
