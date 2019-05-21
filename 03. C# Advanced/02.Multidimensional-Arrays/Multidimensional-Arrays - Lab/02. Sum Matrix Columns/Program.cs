e848bcb21b220a42052389c43c459672504b347d
SlaviSavov <slavisavov1@icloud.com>
2019-05-21 22:20:51 +0300

    Added homework

---
+++ 03. C# Advanced/02.Multidimensional-Arrays/Multidimensional-Arrays - Lab/02. Sum Matrix Columns/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Matrix_Columns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dim = Console.ReadLine()
+              .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+              .Select(int.Parse)
+              .ToArray();
+            int[,] arr = new int[dim[0], dim[1]];
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                int[] tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = tokens[j];
+                }
+            }
+            for (int i = 0; i < arr.GetLength(1); i++)
+            {
+                int sumCol = 0;
+                for (int j = 0; j < arr.GetLength(0); j++)
+                {
+                    sumCol += arr[j, i];
+                }
+                Console.WriteLine(sumCol);
+            }
+        }
+    }
+}
---
+++ 03. C# Advanced/02.Multidimensional-Arrays/Multidimensional-Arrays - Lab/03. Primary Diagonal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dim = int.Parse(Console.ReadLine());
+            int[,] arr = new int[dim,dim];
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                int[] tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = tokens[j];
+                }
+            }
+            int sum = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                sum += arr[i, i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
