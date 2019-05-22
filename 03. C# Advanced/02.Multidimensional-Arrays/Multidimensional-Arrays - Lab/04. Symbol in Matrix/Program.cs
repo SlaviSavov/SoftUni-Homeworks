8c06ed5cf51f659d4117714b60375540d85a1b33
SlaviSavov <slavisavov1@icloud.com>
2019-05-22 11:48:59 +0300

    Added homework and README.md

---
+++ 03. C# Advanced/02.Multidimensional-Arrays/Multidimensional-Arrays - Lab/04. Symbol in Matrix/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _04._Symbol_in_Matrix
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                string input = Console.ReadLine();
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+            char symb = char.Parse(Console.ReadLine());
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (matrix[i,j]==symb)
+                    {
+                        Console.WriteLine($"({i}, {j})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{symb} does not occur in the matrix ");
+        }
+    }
+}
