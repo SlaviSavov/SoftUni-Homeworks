27d20e62affc4737eb699fd7b124df6709ed9496
SlaviSavov <slavisavov1@icloud.com>
2019-06-01 21:55:42 +0300

    Added Homework

---
+++ 03. C# Advanced/04.Streams-Files-and-Directories/Streams, Files and Directories - Lab/03. Word Count/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03._Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fileWords = new StreamReader(Path.Combine("files", "words.txt"));
+            StreamReader fileText = new StreamReader(Path.Combine("files", "text.txt"));
+            StreamWriter fileDestination = new StreamWriter(Path.Combine("files", "output.txt"));
+            Dictionary<string, int> wordsDictionary = new Dictionary<string, int>();
+
+            using (fileWords)
+            {
+                string line = fileWords.ReadLine();
+
+                while (line != null)
+                {
+                    string[] lineWords = line.Split(new char[] { ' ', '\n', '.', '!', '?', '-', ',' },
+                        StringSplitOptions.RemoveEmptyEntries);
+
+                    for (int i = 0; i < lineWords.Length; i++)
+                    {
+                        lineWords[i] = lineWords[i].ToLower();
+
+                        if (!(wordsDictionary.ContainsKey(lineWords[i])))
+                        {
+                            wordsDictionary.Add(lineWords[i], 0);
+                        }
+                    }
+
+                    line = fileWords.ReadLine();
+                }
+
+                using (fileText)
+                {
+                    using (fileDestination)
+                    {
+                        string lineText = fileText.ReadLine();
+
+                        while (lineText != null)
+                        {
+                            string[] lineTextSplited = lineText.Split(new char[] { ' ', '\n', '.', '!', '?', '-', ',' },
+                                StringSplitOptions.RemoveEmptyEntries);
+
+                            for (int j = 0; j < lineTextSplited.Length; j++)
+                            {
+                                lineTextSplited[j] = lineTextSplited[j].ToLower();
+
+                                if (wordsDictionary.ContainsKey(lineTextSplited[j]))
+                                {
+                                    wordsDictionary[lineTextSplited[j]] += 1;
+                                }
+                            }
+
+                            lineText = fileText.ReadLine();
+                        }
+
+                        foreach (var wordCount in wordsDictionary.OrderByDescending(key => key.Value))
+                        {
+                            fileDestination.WriteLine("{0} - {1}", wordCount.Key, wordCount.Value);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ 03. C# Advanced/04.Streams-Files-and-Directories/Streams, Files and Directories - Lab/04. Merge Files/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _04._Merge_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader firstFile = new StreamReader(Path.Combine("files", "FileOne.txt"));
+            StreamReader secondFile = new StreamReader(Path.Combine("files", "FileTwo.txt"));
+            StreamWriter output = new StreamWriter(Path.Combine("files", "output.txt"));
+
+            var result = new List<int>();
+
+            using (firstFile)
+            {
+                string line = string.Empty;
+
+                while ((line = firstFile.ReadLine()) != null)
+                {
+                    int[] lineNum = line.Split(new char[] { ' ', '\n', '.', '!', '?', '-', ',' },
+                        StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+
+                    for (int i = 0; i < lineNum.Length; i++)
+                    {
+                        result.Add(lineNum[i]);
+                    }
+                }
+            }
+
+
+            using (secondFile)
+            {
+                string lineSecondFile = string.Empty;
+
+                while ((lineSecondFile = secondFile.ReadLine()) != null)
+                {
+                    int[] lineNum = lineSecondFile.Split(new char[] { ' ', '\n', '.', '!', '?', '-', ',' },
+                        StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+
+                    for (int i = 0; i < lineNum.Length; i++)
+                    {
+                        result.Add(lineNum[i]);
+                    }
+                }
+            }
+            using (output)
+            {
+                foreach (var num in result.OrderBy(x => x))
+                {
+                    output.WriteLine(num);
+                }
+
+            }
+        }
+    }
+}
