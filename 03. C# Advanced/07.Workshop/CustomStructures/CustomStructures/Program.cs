5c23189f64318129dafd9fa435ae6db2d3fde12e
SlaviSavov <slavisavov1@icloud.com>
2019-06-16 22:09:32 +0300

    Added homework

---
+++ 03. C# Advanced/07.Workshop/CustomStructures/CustomStructures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new CustomList();
+        }
+    }
+}
