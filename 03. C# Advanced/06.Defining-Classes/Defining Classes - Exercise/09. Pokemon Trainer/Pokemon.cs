6f0f5538e4f4eaad6ae3d073808c9797e39f58af
SlaviSavov <slavisavov1@icloud.com>
2019-06-16 08:47:13 +0300

    Added homework

---
+++ 03. C# Advanced/06.Defining-Classes/Defining Classes - Exercise/09. Pokemon Trainer/Pokemon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09._Pokemon_Trainer
+{
+    public class Pokemon
+    {
+        private string name;
+        private string element;
+        private int health;
+
+        public string Element { get; set; }
+        public int Health { get; set; }
+
+        public Pokemon(string name,string element,int health)
+        {
+            this.name = name;
+            Element = element;
+            Health = health;
+        }
+        public void ReduceHealth()
+        {
+            this.health -= 10;
+        }
+    }
+}
