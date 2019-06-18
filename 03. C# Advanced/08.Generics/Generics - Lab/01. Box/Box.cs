e149eada2403e6c05b47eb642ce8eb23f99b7cb1
SlaviSavov <slavisavov1@icloud.com>
2019-06-19 00:33:13 +0300

    Added homework

---
+++ 03. C# Advanced/08.Generics/Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> boxItems;
+
+        public int Count
+        {   
+            get => boxItems.Count;
+        }
+
+        public Box()
+        {
+            boxItems = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            boxItems.Add(element);
+        }
+        public T Remove()
+        {
+            if (Count>0)
+            {
+                T element = boxItems.LastOrDefault();
+                boxItems.RemoveAt(Count - 1);
+
+                return element;
+            }
+
+            throw new InvalidOperationException("Can not remove element from empty collection.");
+
+        }
+         
+    }
+}
---
+++ 03. C# Advanced/08.Generics/Generics - Lab/03. Generic Scale/EqualityScale.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T> where T:IComparable
+    {
+        private T Left;
+        private T Right;
+
+        public EqualityScale(T left,T right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+
+        public bool AreEqual()
+        {
+            return Left.CompareTo(Right) == 0;
+        }
+        public void WhichIsHavier()
+        {
+            int result = Left.CompareTo(Right);
+            if (result==0)
+            {
+                Console.WriteLine("Right and Left are equal.");
+            }
+            else if (result>0)
+            {
+                Console.WriteLine("Left is havier.");
+            }
+            else if(result<0)
+            {
+                Console.WriteLine("Right is havier.");
+            }
+        }
+    }
+}
+        
