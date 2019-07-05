0887ea1a33542a5a8d7d872026d6bd5d23f02e22
SlaviSavov <slavisavov1@icloud.com>
2019-07-05 22:24:50 +0300

    Added homework

---
+++ 04. C# OOP/Working with Abstraction - Lab/1. Rhombus of Stars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1._Rhombus_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= count; i++)
+            {
+                printRow(i,count);
+            }
+
+            for (int i = count -1; i > 0; i--)
+            {
+                printRow(i, count);
+            }
+        }
+
+        private static void printRow(int stars, int total)
+        {
+            int leadingSpaces = total - stars;
+            Console.Write(new string(' ',leadingSpaces));
+
+            for (int i = 0; i < stars; i++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
