e758233e6bbead6c5162a24b5071d761eb4547fa
SlaviSavov <slavisavov1@icloud.com>
2019-07-06 09:40:26 +0300

    Added homework

---
+++ 04. C# OOP/Working with Abstraction - Lab/PointInRectangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace PointInRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rectangleCoordinate = ParseInput();
+
+            Point leftTop = new Point(rectangleCoordinate[0], rectangleCoordinate[1]);
+            Point bottomRight = new Point(rectangleCoordinate[2], rectangleCoordinate[3]);
+
+            Rectangle rectangle = new Rectangle(leftTop, bottomRight);
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var inputCoordinate = ParseInput();
+
+                Point point = new Point(inputCoordinate[0], inputCoordinate[1]);
+
+                Console.WriteLine(rectangle.Contains(point));
+            }
+
+        }
+
+        private static int[] ParseInput()
+        {
+            return Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
---
+++ 04. C# OOP/Working with Abstraction - Lab/PointInRectangle/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointInRectangle
+{
+    public class Rectangle
+    {
+        public Point TopLeft { get; set; }
+        public Point BottomRight { get; set; }
+
+        public Rectangle(Point topLeft,Point bottomRight)
+        {
+            TopLeft = topLeft;
+            BottomRight = bottomRight;
+        }
+        public bool Contains(Point point)
+        {
+            bool insideByX = point.X >= TopLeft.X 
+                && point.X <= BottomRight.X;
+
+            bool insideByY = point.Y >= TopLeft.Y
+                && point.Y <= BottomRight.Y;
+
+            return insideByX && insideByY;
+        }
+    }
+}
