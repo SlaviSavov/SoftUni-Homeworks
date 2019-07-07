7f39784db0637b46f632c3c8a4702602193730bc
SlaviSavov <slavisavov1@icloud.com>
2019-07-07 19:12:41 +0300

    Added homework

---
+++ 04. C# OOP/Working with Abstraction Exercises/P01_RawData/Engine.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P01_RawData
+{
+ public    class Engine
+    {
+        private List<Car> cars;
+
+        public Engine()
+        {
+            cars = new List<Car>();
+        }
+        public void Run()
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] parameters = Console.ReadLine()
+                    ?.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                Car car = CreateCar(parameters);
+
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+            if (command == "fragile")
+            {
+                List<string> fragile = cars
+                    .Where(x => x.CargoType == "fragile" && x.Tires.Any(y => y.Pressure < 1))
+                    .Select(x => x.Model)
+                    .ToList();
+
+                PrintInfo(fragile);
+            }
+            else
+            {
+                List<string> flamable = cars
+                    .Where(x => x.CargoType == "flamable" && x.EnginePower > 250)
+                    .Select(x => x.Model)
+                    .ToList();
+
+                PrintInfo(flamable);
+            }
+
+        }
+
+        private void PrintInfo(List<string> cars)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, cars));
+        }
+
+        private Car CreateCar(string[] parameters)
+        {
+            string model = parameters[0];
+            int engineSpeed = int.Parse(parameters[1]);
+            int enginePower = int.Parse(parameters[2]);
+            int cargoWeight = int.Parse(parameters[3]);
+            string cargoType = parameters[4];
+
+
+            double tire1Pressure = double.Parse(parameters[5]);
+            int tire1Age = int.Parse(parameters[6]);
+            var firstTire = new Tire(tire1Age, tire1Pressure);
+
+            double tire2Pressure = double.Parse(parameters[7]);
+            int tire2Age = int.Parse(parameters[8]);
+            var secondTire = new Tire(tire2Age, tire2Pressure);
+
+            double tire3Pressure = double.Parse(parameters[9]);
+            int tire3Age = int.Parse(parameters[10]);
+            var thirdTire = new Tire(tire3Age, tire3Pressure);
+
+            double tire4Pressure = double.Parse(parameters[11]);
+            int tire4Age = int.Parse(parameters[12]);
+            var fourthTire = new Tire(tire4Age, tire4Pressure);
+
+            var car = new Car(model, engineSpeed, enginePower,
+                cargoWeight, cargoType, firstTire, secondTire, thirdTire, fourthTire);
+
+            return car;
+        }
+    }
+}
---
+++ 04. C# OOP/Working with Abstraction Exercises/P03_JediGalaxy/Engine.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P03_JediGalaxy
+{
+    public class Engine
+    {
+        private int[,] matrix;
+
+        private long totalSum;
+        public void Run()
+        {
+            int[] dimensions = Splitter(Console.ReadLine());
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            this.InitializeMatrix(rows, cols);
+
+            string command = Console.ReadLine();
+
+            while (command != "Let the Force be with you")
+            {
+                int[] playerCordinates = Splitter(command);
+
+                int[] evilCordinates = Splitter(Console.ReadLine());
+
+                int evilRow = evilCordinates[0];
+                int evilCol = evilCordinates[1];
+
+                MoveElivToTopLeftCorner(evilRow, evilCol);
+
+                int playerRow = playerCordinates[0];
+                int playerCol = playerCordinates[1];
+
+                MovePlayerToRightCorner(playerRow, playerCol);
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(totalSum);
+
+
+        }
+
+        private static int[] Splitter(string input)
+        {
+            return input
+    .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+        }
+
+        private void MovePlayerToRightCorner(int playerRow, int playerCol)
+        {
+            while (playerRow >= 0 && playerCol < matrix.GetLength(1))
+            {
+                if (IsInside(playerRow, playerCol))
+                {
+                    totalSum += matrix[playerRow, playerCol];
+                }
+
+                playerCol++;
+                playerRow--;
+            }
+        }
+
+        private void MoveElivToTopLeftCorner(int evilRow, int evilCol)
+        {
+            while (evilRow >= 0 && evilCol >= 0)
+            {
+                if (this.IsInside(evilRow, evilCol))
+                {
+                    matrix[evilRow, evilCol] = 0;
+                }
+                evilRow--;
+                evilCol--;
+            }
+        }
+
+        private bool IsInside(int targetRow, int targetCol)
+        {
+            return targetRow >= 0 && targetRow < matrix.GetLength(0)
+                && targetCol >= 0 && targetCol < matrix.GetLength(1);
+        }
+
+        private void InitializeMatrix(int rows, int cols)
+        {
+            matrix = new int[rows, cols];
+            int value = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = value++;
+                }
+            }
+        }
+    }
+}
