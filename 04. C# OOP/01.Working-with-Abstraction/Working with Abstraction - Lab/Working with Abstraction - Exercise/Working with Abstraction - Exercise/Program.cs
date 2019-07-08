f473ef1e0eca5253a432e0d9a40aa885c165ed9c
SlaviSavov <slavisavov1@icloud.com>
2019-07-08 08:50:01 +0300

    Added homework

---
+++ 04. C# OOP/01.Working-with-Abstraction/Working with Abstraction - Lab/Working with Abstraction - Exercise/Working with Abstraction - Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Working_with_Abstraction___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
