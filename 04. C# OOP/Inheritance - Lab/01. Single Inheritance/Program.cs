18c4baa21fb1cd6dcdd46ce443f236501662d898
SlaviSavov <slavisavov1@icloud.com>
2019-07-10 22:14:31 +0300

    Added homework

---
+++ 04. C# OOP/Inheritance - Lab/01. Single Inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Single_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
+        }
+    }
+}
---
+++ 04. C# OOP/Inheritance - Lab/01. Single Inheritance/Puppy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._Single_Inheritance
+{
+    public class Puppy:Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+    }
+}
