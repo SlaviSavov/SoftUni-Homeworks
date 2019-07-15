1adc0e5d536247e6c988cba4c1b87e46f196ee48
SlaviSavov <slavisavov1@icloud.com>
2019-07-15 20:32:33 +0300

    Added homework

---
+++ 04. C# OOP/03.Encapsulation/Encapsulation-Exercises/P05_FootballTeamGenerator/Models/Stat.cs
@@ -0,0 +1,105 @@
+﻿using P05_FootballTeamGenerator.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05_FootballTeamGenerator.Models
+{
+    public class Stat
+    {
+        private const int MIN_STAT_VALUE = 0;
+        private const int MAX_STAT_VALUE = 100;
+
+        private int endurance;
+        private int sprint;
+        private int dribble;
+        private int passing;
+        private int shooting;
+
+        public Stat(int enduranse, int sprint, int dribble, int passing, int shooting)
+        {
+            Endurance = enduranse;
+            Spint = sprint;
+            Dribble = dribble;
+            Passing = passing;
+            Shooting = shooting;
+        }
+
+        public int Endurance
+        {
+            get
+            {
+                return this.endurance;
+            }
+            private set
+            {
+                ValidateStat(value, nameof(this.Endurance));
+                this.endurance = value;
+            }
+        }
+        public int Spint
+        {
+            get
+            {
+                return this.sprint;
+            }
+            private set
+            {
+                ValidateStat(value, nameof(this.Spint));
+                this.sprint = value;
+            }
+        }
+        public int Dribble
+        {
+            get
+            {
+                return this.dribble;
+            }
+            private set
+            {
+                ValidateStat(value, nameof(this.Dribble));
+                this.dribble = value;
+            }
+        }
+        public int Passing
+        {
+            get
+            {
+                return this.passing;
+            }
+            private set
+            {
+                ValidateStat(value, nameof(this.Passing));
+                this.passing = value;
+            }
+        }
+        public int Shooting
+        {
+            get
+            {
+                return this.shooting;
+            }
+            private set
+            {
+                ValidateStat(value, nameof(this.Shooting));
+                this.shooting = value;
+            }
+        }
+        public double OverallStat =>
+            ((Endurance + Spint + Dribble + Passing + Shooting) / 5.0);
+
+
+
+
+        private void ValidateStat(int value, string name)
+        {
+            if (value < MIN_STAT_VALUE || value > MAX_STAT_VALUE)
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.InvalidStatException,
+                    name, MIN_STAT_VALUE, MAX_STAT_VALUE));
+            }
+        }
+
+
+    }
+}
---
+++ 04. C# OOP/03.Encapsulation/Encapsulation-Exercises/P05_FootballTeamGenerator/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using P05_FootballTeamGenerator.Core;
+using System;
+
+namespace P05_FootballTeamGenerator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
