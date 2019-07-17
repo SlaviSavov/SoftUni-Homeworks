8bf59730c34fc3fff82e4ee1506e10f6cd8b0535
SlaviSavov <slavisavov1@icloud.com>
2019-07-17 20:42:38 +0300

    Added homework

---
+++ 04. C# OOP/04.Interfaces-and-Abstraction/Interfaces and Abstraction - Lab/P02_Cars/Tesla.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : Car, IElectricCar
+    {
+        public int Battery { get; set; }
+
+        public Tesla(string model,string color,int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{Color} Tesla {Model} With {Battery} Batteries");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
