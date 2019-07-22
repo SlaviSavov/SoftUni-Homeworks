fbd7dbb229208958d7f75ea243b2ea9544f5d99c
SlaviSavov <slavisavov1@icloud.com>
2019-07-22 21:12:09 +0300

    added homework

---
+++ 04. C# OOP/05.Polymorphism/Polymorphism-Lab/P02_Animals/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public string Name { get;private set; }
+        public string FavouriteFood { get;private set; }
+
+        public Animal(string name,string food)
+        {
+            Name = name;
+            FavouriteFood = food;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my fovourite food is {FavouriteFood }";
+        }
+    }
+}
