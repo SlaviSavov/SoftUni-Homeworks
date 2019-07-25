3e6c9c2acb6de7e9d638b9aed113b2edc5413654
SlaviSavov <slavisavov1@icloud.com>
2019-07-25 23:52:44 +0300

    Added homework

---
+++ 04. C# OOP/06.SOLID/SOLID_Exercise/Logger/Models/Appenders/FileAppender.cs
@@ -0,0 +1,34 @@
+﻿using Logger.Models.Contracts;
+using Logger.Models.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger.Models.Appenders
+{
+    public class FileAppender : IAppender
+    {
+        private int messagesAppended;
+        public FileAppender(ILayout layout, Level level, IFile file)
+        {
+            Layout = layout;
+            Level = level;
+            File = file;
+        }
+        public ILayout Layout { get; private set; }
+
+        public Level Level { get; private set; }
+
+        public IFile File { get;private set; }
+
+        public void Append(IError error)
+        {
+            string formattedMessage = this.File
+                .Write(this.Layout, error);
+
+            System.IO.File.AppendAllText(this.File.Path,
+                formattedMessage);
+            this.messagesAppended++;
+        }
+    }
+}
---
+++ 04. C# OOP/06.SOLID/SOLID_Exercise/Logger/Models/Layouts/XMLLayout.cs
@@ -0,0 +1,24 @@
+﻿using Logger.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger.Models.Layouts
+{
+    public class XMLLayout : ILayout
+    {
+        public string Format => GetForamt();
+
+        private string GetForamt()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("<log>")
+                .AppendLine("\t<date>{0}</date>")
+                .AppendLine("\t<level>{1}</level>")
+                .AppendLine("\t<message>{2}</message>")
+                .AppendLine("</log>");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
