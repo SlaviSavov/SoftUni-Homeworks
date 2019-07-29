f5bb03fb497bbf95d8c7cbd1dd574579c4533861
SlaviSavov <slavisavov1@icloud.com>
2019-07-29 22:09:10 +0300

    dded homework

---
+++ 04. C# OOP/07.Reflection-and-Attributes/Reflection-and-Attributes-Lab/P02_Attribute/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P02_Attribute
+{
+    [Author("Ventsi")]
+    class StartUp
+    {
+        [Author("Gosho")]
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
